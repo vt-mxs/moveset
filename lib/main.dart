3e54bdc7793063c28eeeec02b8e04d4e38b99020
vt-mxs <maximusvitor88@gmail.com>
2025-01-15 16:00:29 -0300

    refactor: limpar método main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:moveset/core/constants/app_routes.dart';
+import 'package:moveset/screens/login_screen.dart';
 import 'package:moveset/screens/welcome_screen.dart';
 
-void main() => runApp(MaterialApp(home: WelcomeScreen()));
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      initialRoute: AppRoutes.welcome,
+
+      routes: {
+        AppRoutes.welcome: (_) => const WelcomeScreen(),
+        AppRoutes.login: (_) => const LoginScreen(),
+        //'/register': (_) => TODO:implementar tela de registro depois
+      },
+    );
+  }
+}
