fe84148ab9a4011aacba7b4c66dca7b90173c388
vt-mxs <maximusvitor88@gmail.com>
2025-01-16 17:50:27 -0300

    refactor:passar pra stateful

--- lib/core/widgets/custom_input_field.dart
+++ lib/core/widgets/custom_input_field.dart
@@ -2,11 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:moveset/core/constants/app_colors.dart';
 import 'package:moveset/core/widgets/responsive_text.dart';
 
-class CustomInputField extends StatelessWidget {
+class CustomInputField extends StatefulWidget {
   final String label;
   final bool isPassword;
   final TextEditingController? controller;
 
+  const CustomInputField({
+    super.key,
+    required this.label,
+    this.isPassword = false,
+    this.controller,
+  });
+
+  @override
+  _CustomInputFieldState createState() => _CustomInputFieldState();
+}
+
+class _CustomInputFieldState extends State<CustomInputField> {
+  bool _isPasswordVisible = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      obscureText: widget.isPassword && !_isPasswordVisible,
+      
+      decoration: InputDecoration(
+        label: ResponsiveText(
+          text: widget.label,
+          fontSize: 5,
+          style: TextStyle(color: AppColors.mainGray),
+        ),
+        
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18),
+          borderSide: BorderSide(color: AppColors.mainShowCaseBlue),
+        ),
+        
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(18),
+          borderSide: BorderSide(color: AppColors.mainShowCaseBlue),
+        ),
+
+        suffixIcon: widget.isPassword
+            ? IconButton(
+                icon: Icon(
+                  _isPasswordVisible
+                      ? Icons.visibility
+                      : Icons.visibility_off,
+                  color: AppColors.mainGray,
+                ),
+                onPressed: () {
+                  setState(() {
+                    _isPasswordVisible = !_isPasswordVisible;
+                  });
+                },
+              )
+            : null,
+      ),
+    );
+  }
+}
+/*
+class CustomInputField extends StatelessWidget {
+  final String label;
+  final bool isPassword;
+  final TextEditingController? controller;
 
   const CustomInputField({
     super.key,
@@ -20,6 +81,7 @@ class CustomInputField extends StatelessWidget {
     return TextField(
       controller: controller,
       obscureText: isPassword,
+      
       decoration: InputDecoration(
         label: ResponsiveText(
           text: label,
@@ -33,10 +95,12 @@ class CustomInputField extends StatelessWidget {
         ),
 
         focusedBorder: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(18),
+          borderRadius: BorderRadius.circular(18),  
           borderSide: BorderSide(color: AppColors.mainShowCaseBlue)
         )
       ),
     );
   }
 }
+
+*/
