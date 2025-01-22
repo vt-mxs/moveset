c4ece8cb80746ec83799053cf7b5df5ed86fd8b2
vt-mxs <maximusvitor88@gmail.com>
2025-01-22 18:50:31 -0300

    feat: fixar orientação da tela em modo retrato

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:moveset/core/constants/app_routes.dart';
 import 'package:moveset/view/auth/login_screen.dart';
 import 'package:moveset/view/auth/register_screen.dart';
 import 'package:moveset/view/auth/welcome_screen.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]).then((_) => runApp(MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -13,7 +21,6 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       initialRoute: AppRoutes.welcome,
-
       routes: {
         AppRoutes.welcome: (_) => const WelcomeScreen(),
         AppRoutes.login: (_) => const LoginScreen(),
