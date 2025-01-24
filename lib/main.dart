6863aa2fe3c944b4a0245f86963e0e99917430c9
vt-mxs <maximusvitor88@gmail.com>
2025-01-24 16:05:01 -0300

    feat: add config do provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,30 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:moveset/core/constants/app_routes.dart';
+import 'package:moveset/firebase_options.dart';
 import 'package:moveset/view/auth/login_screen.dart';
 import 'package:moveset/view/auth/register_screen.dart';
 import 'package:moveset/view/auth/welcome_screen.dart';
+import 'package:moveset/viewmodel/auth_viewmodel.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown
-  ]).then((_) => runApp(MyApp()));
+    DeviceOrientation.portraitUp, DeviceOrientation.portraitDown
+  ]).then((_) => runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthViewmodel())
+      ],
+      child: MyApp(),
+    )
+  ));
+  
 }
 
 class MyApp extends StatelessWidget {
@@ -24,7 +37,7 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.welcome: (_) => const WelcomeScreen(),
         AppRoutes.login: (_) => const LoginScreen(),
-        AppRoutes.register: (_) => const RegisterScreen()
+        AppRoutes.register: (_) => RegisterScreen()
       },
     );
   }
