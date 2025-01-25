cd5c9436c961b06c1730648024825816016f82ff
vt-mxs <maximusvitor88@gmail.com>
2025-01-25 13:27:12 -0300

    feat: adcionar rota pra home

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -2,4 +2,5 @@ class AppRoutes {
   static const String welcome = '/';
   static const String login = '/login';
   static const String register = '/register';
+  static const String home = '/home';
 }
