c6d8a35d15c8be1934bfa4548bf8d45bca29740c
vt-mxs <maximusvitor88@gmail.com>
2025-01-25 15:18:41 -0300

    refactor:remover notifys desnecessarios

--- lib/viewmodel/auth_viewmodel.dart
+++ lib/viewmodel/auth_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 import 'package:moveset/core/utils/response.dart';
 import 'package:moveset/service/auth_service.dart';
 
+
 class AuthViewmodel with ChangeNotifier {
   final AuthService _service = AuthService();
   bool _isLoading = false;
@@ -9,20 +10,33 @@ class AuthViewmodel with ChangeNotifier {
   bool get isLoading => _isLoading;
 
   Future<String?> register(String name, String email, String password) async {
-    _isLoading = true;
     notifyListeners();
 
     Response result = await _service.register(name, email, password);
-    
+
+    notifyListeners();
+
     if (result.isFailure) {
-      _isLoading = false;
+      return result.message;
+    }
+
+    notifyListeners();
+
+    return null; // sucesso
+  }
+
+  Future<String?> login(String email, String password) async {
+    notifyListeners();
+
+    Response result = await _service.login(email, password);
+
+    if (result.isFailure) {
       notifyListeners();
       return result.message;
     }
 
-    _isLoading = false;
     notifyListeners();
 
-    return null; // sucesso
+    return null;
   }
 }
