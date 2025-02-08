52fa99de5998d995d20b42c636640c3ab524eac1
vt-mxs <maximusvitor88@gmail.com>
2025-02-08 12:52:43 -0300

    feat: adcionar conversão via firebase

--- lib/model/phase.dart
+++ lib/model/phase.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:moveset/model/exercise.dart';
 
 class Phase {
@@ -10,6 +11,36 @@ class Phase {
     required this.name,
     required this.exercises
   });
+
+  /// Converte um documento Firestore para um objeto `Phase`
+  static Future<Phase> fromFirestore(DocumentSnapshot doc) async {
+    String id = doc.id;
+    Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+
+    if (data == null) {
+      throw Exception('Documento vazio.');
+    }
+
+    // Busca os exercícios na subcoleção "exercises"
+    List<Exercise> exercises = await FirebaseFirestore.instance
+        .collection('workouts')
+        .doc(doc.reference.parent.parent!.id) // ID do workout
+        .collection('phases')
+        .doc(id)
+        .collection('exercises')
+        .get()
+        .then((querySnapshot) {
+      return querySnapshot.docs.map((exerciseDoc) {
+        return Exercise.fromMap(exerciseDoc.data(), exerciseDoc.id);
+      }).toList();
+    });
+
+    return Phase(
+      id: id,
+      name: data['name'],
+      exercises: exercises
+    );
+  }
 
   factory Phase.fromMap(Map<String, dynamic> map, String id) {
     return Phase(
--- lib/model/workout.dart
+++ lib/model/workout.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:moveset/model/phase.dart';
 
 class Workout {
@@ -10,6 +11,31 @@ class Workout {
     required this.name,
     required this.phases
   });
+
+  static Future<Workout> fromFirestore(DocumentSnapshot doc) async{
+    String id = doc.id;
+    Map<String, dynamic>? data = doc.data() as Map<String, dynamic>?;
+
+    if(data == null) return Future.error(Exception('Workout não existe'));
+
+    List<Phase> phases = await FirebaseFirestore.instance
+        .collection('workouts')
+        .doc(id)
+        .collection('phases')
+        .get()
+        .then((querySnapshot) async {
+          return Future.wait(querySnapshot.docs.map((phaseDoc) async {
+            return await Phase.fromFirestore(phaseDoc);
+          }
+        ).toList());
+    });
+
+    return Workout(
+      id: id,
+      name: data['name'],
+      phases: phases
+    );
+  }
 
   factory Workout.fromMap(Map<String, dynamic> map, String id) {
     return Workout(
