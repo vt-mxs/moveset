4c99ea821151b44767d05048e8c2e3599ab3cb5a
vt-mxs <maximusvitor88@gmail.com>
2025-02-13 17:00:14 -0300

    feat: criar estrutura básica da tela de muscle up

--- lib/view/home/workout_screens/muscle_up_workout_screen.dart
+++ lib/view/home/workout_screens/muscle_up_workout_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:moveset/core/widgets/responsive_text.dart';
+import 'package:moveset/model/phase.dart';
 import 'package:moveset/model/workout.dart';
 import 'package:moveset/view/home/home_widgets/rounded_header.dart';
 
@@ -8,13 +10,37 @@ class MuscleUpWorkoutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
+    final double screenHeight = MediaQuery.of(context).size.height;    
     return Scaffold(
       body: Stack(
         children: [
           RoundedHeader(
             title: 'Muscle Up'
-          )
+          ),
+
+          Container(
+            padding: EdgeInsets.only(top: screenHeight * 0.3),
+            
+            child: ListView.builder(
+              itemCount: workout.phases.length,
+              itemBuilder: (context, index) {
+                final Phase phase = workout.phases[index];
+                return Card(
+                  elevation: 4,
+                  margin: EdgeInsets.all(screenHeight * 0.02),
+                  child: ListTile(
+                    title: ResponsiveText(
+                      text: phase.name,
+                      fontSize: 5
+                    ),
+                    onTap: () {
+                      print(':)');
+                    },
+                  ),
+                );
+              }
+            ),
+          ),
         ],
       ),
     );
